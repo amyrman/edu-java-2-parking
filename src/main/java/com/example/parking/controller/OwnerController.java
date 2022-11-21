bc919eea0eb998dd0f3048fd8129febb457d009a
Anders Myrman <amyhrman@gmail.com>
2022-11-21 14:05:47 +0100

    Spring Boot boilerplate

---
+++ src/main/java/com/example/parking/controller/OwnerController.java
@@ -0,0 +1,4 @@
+package com.example.parking.controller;
+
+public class OwnerController {
+}
---
+++ src/test/java/com/example/parking/Lab2ParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2ParkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
